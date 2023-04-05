8fd8de51fafdc3e23595690611f0e77a5d3f4980
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-04-05 12:48:36 -0500

    Update game.mjs

--- public/game.mjs
+++ public/game.mjs
@@ -1,58 +1,129 @@
+import Player from './Player.mjs';
+import Collectible from './Collectible.mjs';
 import { dimension } from './dimension.mjs';
+const socket = io();
 
-class Player {
-  constructor({x, y, score, id, radius = 30}) {
-    this.id = id;
-    this.score = score;
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-  }
 
-  movePlayer(dir, speed) {
-    switch(dir) {
-      case 'up':
-        this.y  = Math.max(dimension.minY+this.radius, this.y - speed);
-        break;
-      case 'down':
-        this.y  = Math.min(dimension.maxY-this.radius, this.y + speed);
-        break;
-      case 'left':
-        this.x  = Math.max(dimension.minX+this.radius, this.x - speed);
-        break;
-      case 'right':
-        this.x  = Math.min(dimension.maxX-this.radius, this.x + speed);
-        break;
-    }
-  }
+let tick;
+let playersList = [];
+let shrimpEntity;
+let playerEntity;
+let sharkEntity;
 
-  collision(item) {
-    var dx = this.x - item.x;
-    var dy = this.y - item.y;
-    var distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < this.radius + item.radius) {
-      return true;
-    }
-    return false;
-  }
 
-  draw(context, img){
-    context.drawImage(img, this.x-this.radius, this.y-this.radius, 2*this.radius, 2*this.radius);
-  }
+const canvas = document.getElementById('game-window');
+const context = canvas.getContext('2d');
 
-  calculateRank(arr) {
-     const sort = arr.sort((a, b) => b.score - a.score);
-    let position = 0
-    sort.forEach((player, index) => {
-      if(this.id === player.id) position = index+1;
-    });
 
-    return `Rank: ${position} / ${arr.length}`;
-  }
+let fishImage = new Image();
+let shrimpImage = new Image();
+let sharkImage = new Image();
+
+const init = () => {
+  // get images
+  fishImage.src = 'public/img/fish.png';
+  shrimpImage.src = 'public/img/shrimp.png';
+  sharkImage.src = 'public/img/shark.png';
   
-}
-try {
-  module.exports = Player;
-} catch(e) {}
+  // create user
+  socket.on('init', ({ id, players, shrimp, shark }) => {
+    console.log(id, players,shrimp,shark);
+    shrimpEntity = new Collectible(shrimp);
+    playerEntity = players.filter(x => x.id === id)[0];
+    playerEntity = new Player(playerEntity);
+    sharkEntity = new Player(shark);
+  
+    playersList = players
 
-export default Player;
+
+    document.onkeydown = e => {
+      let  dir = null
+      switch(e.keyCode) {
+        case 87:
+        case 38:
+           dir = 'up';
+           break;
+        case 83:
+        case 40:
+           dir = 'down';
+           break;
+        case 65:
+        case 37:
+           dir = 'left';
+           break;
+        case 68:
+        case 39:
+           dir = 'right';
+           break;   
+      }
+      if (dir) {
+        playerEntity.movePlayer(dir, 10);
+        socket.emit('update', playerEntity);
+      }
+    }
+  
+    // update
+    socket.on('update', ({players:players,shark:shark, shrimp:shrimp,player:player}) => {
+      sharkEntity = new Player(shark)
+      playersList = players;
+      shrimpEntity = new Collectible(shrimp)
+      if (player) {
+        if (player.id === playerEntity.id) {
+          playerEntity= new Player(player);
+        }
+      }
+      
+    });
+  
+  });
+  
+  window.requestAnimationFrame(update); 
+}
+
+const update = () => {
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Set background color
+  context.fillStyle = '#17202A';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Create border for play field
+  context.strokeStyle = '#45b6fe';
+  context.strokeRect(dimension.minX, dimension.minY, dimension.arenaSizeX, dimension.arenaSizeY);
+
+  // Controls text
+  context.fillStyle = '#FDFEFE';
+  context.font = `20px 'Papryus'`;
+  context.textAlign = 'center';
+  context.fillText('Controls', 80, 23);
+  context.textAlign = 'center';
+  context.fillText('WASD', 80, 45);
+
+  // Game title
+  context.font = `40px 'Papyrus'`;
+  context.fillText('Fish Life Simulator', 300, 45, 300);
+
+  if (playerEntity) {
+    playerEntity.draw(context,fishImage);
+    context.font = `26px 'Papyrus'`;
+    context.fillText(playerEntity.calculateRank(playersList), 560, 45);
+    playersList.forEach((player)=> {
+       if (player.id !== playerEntity.id) {
+         let p = new Player(player);
+         p.draw(context, fishImage);
+       }
+    });
+    if (shrimpEntity) {
+      shrimpEntity.draw(context,shrimpImage);
+    }
+    if (sharkEntity) {
+      sharkEntity.draw(context,sharkImage);
+    }
+  }
+
+ 
+  tick = requestAnimationFrame(update);
+}
+
+init();
